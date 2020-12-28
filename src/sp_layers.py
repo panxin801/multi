d5a985bc8c8ff66a6c680e70ab7b0bd1426a2f29
panxin801 <1457353243@qq.com>
2020-12-28 10:15:16 +0800

    find a bug temp fixed

--- src/sp_layers.py
+++ src/sp_layers.py
@@ -144,7 +144,10 @@ class SPLayer(nn.Module):
         retCC = self.CConv(padded_features)
         retCC = self.CConv(retCC)
         padded_features = self.LastCConv(retCC)
-        padded_features = padded_features.squeeze()
+        # New added line seems dim mismatch. 
+        # Bug: Expected 4-dimensional input for 4-dimensional weight [1, 2, 1, 1], but got 3-dimensional input of size [2, 2818, 257] instead
+        if padded_features.dim() == 4:
+            padded_features = padded_features.squeeze()
         padded_features = self.LastConv(padded_features).squeeze()
         # seems like [25,736,257]
 
